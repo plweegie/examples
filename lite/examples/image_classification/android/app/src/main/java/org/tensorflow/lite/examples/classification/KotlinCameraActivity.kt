f6cfa8756517c5b72cd0578a91c0b77d9dcfe6a0
Jszymanski <jan.kazimierz.szymanski@gmail.com>
2020-05-06 17:49:12 +0100

    Work in progress

---
+++ lite/examples/image_classification/android/app/src/main/java/org/tensorflow/lite/examples/classification/KotlinCameraActivity.kt
@@ -0,0 +1,12 @@
+package org.tensorflow.lite.examples.classification
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class KotlinCameraActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kotlin_camera)
+    }
+}
